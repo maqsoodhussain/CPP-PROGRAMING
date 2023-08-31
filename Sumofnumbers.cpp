cfc4fa77b758cd89bf5e8e69cc643be39964bf33
MAQSOOD HUSSAIN WANI <83625735+maqsoodhussain@users.noreply.github.com>
2023-08-31 20:46:25 +0530

    Add files via upload

---
+++ Sumofnumbers.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    /*
+     sum of number with out geting output from user
+    */
+
+/* code is given blow*/
+
+/*
+   int num1 = 20;
+   int num2=11;
+   int sum;
+
+   sum = num1 + num2;
+*/
+
+/*sum of numbers, numbers given by user */
+
+    int num1, num2;
+    int sum; 
+   
+
+ 
+   
+    cout<<"Enter value for first number"<<endl;
+    cin>>num1;
+
+    cout<<"Enter value for Second number"<<endl;
+    cin>>num2;
+
+    sum = num1 + num2;
+
+    cout<<"Sum of two numbers = "<<sum<<endl;
+   
+    return 0;
+}
