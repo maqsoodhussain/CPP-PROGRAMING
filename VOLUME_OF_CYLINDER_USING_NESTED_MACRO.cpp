dfad76c0dd990e2f9f2629a3c571f0c9510fa787
MAQSOOD HUSSAIN WANI <83625735+maqsoodhussain@users.noreply.github.com>
2023-10-12 08:19:25 +0530

    Create VOLUME_OF_CYLINDER_USING_NESTED_MACRO.cpp

---
+++ VOLUME_OF_CYLINDER_USING_NESTED_MACRO.cpp
@@ -0,0 +1,21 @@
+
+
+ #include <iostream>
+
+ using namespace std;
+
+//formulla for volume of cylinder pi*r*r*h
+ #define PI 3.14
+#define SQUARE (r) (r*r) 7 #define VOL_CYLD(r,h) (PI SQUARE(r) *h) *
+
+int main() {
+
+// Write C++ code here
+
+int radius = 10;
+
+int height 5; 
+cout<<" Volume of cylinder is "<<VOL_CYLD(radius, height);
+
+return 0;
+}
